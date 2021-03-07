77c7d0d4e0885bdab8da57ea48ced2ddb9e2ca08
David Levanon <dvdlevanon@gmail.com>
2021-03-07 12:19:17 +0200

    add nginx port to monitor

--- code/monitor/dudemo-monitor.sh
+++ code/monitor/dudemo-monitor.sh
@@ -2,4 +2,4 @@
 
 [ -z "$DUDEMO_SERVER_PORT" ] && export DUDEMO_SERVER_PORT=9090
 
-tcpdump -n -i any dst port 3306 or dst port "$DUDEMO_SERVER_PORT"
+tcpdump -n -i any dst port 3306 or dst port "$DUDEMO_SERVER_PORT" or dst port 4040
