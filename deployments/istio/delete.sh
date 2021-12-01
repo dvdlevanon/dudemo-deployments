0162e56ba429758e6d15de12266a9389aa9970f6
David Levanon <dvdlevanon@gmail.com>
2021-12-02 01:03:03 +0200

    support configurable namespace when removing istio

--- deployments/istio/delete.sh
+++ deployments/istio/delete.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+[ -z "$DUDEMO_KUBE_NAMESPACE" ] && export DUDEMO_KUBE_NAMESPACE="default"
+
 istioctl manifest generate --set profile=demo | kubectl delete --ignore-not-found=true -f -
 kubectl delete namespace istio-system
-kubectl label namespace default istio-injection-
+kubectl label namespace "$DUDEMO_KUBE_NAMESPACE" istio-injection-
 
 pushd ../kubernetes || exit 1
 ./delete.sh
